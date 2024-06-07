c1610f4016906550c6897ce665563171e4cfdad9
bindhupri <116120716+bindhupri@users.noreply.github.com>
2024-06-07 09:36:55 -0500

    Update Clearanceprocessor_beam_narayana.py

--- Clearanceprocessor_beam_narayana.py
+++ Clearanceprocessor_beam_narayana.py
@@ -66,14 +66,9 @@ class GroupByAndCount(beam.DoFn):
             'ProductCount': len(set(prod_ids))
         }
 
-class DeduplicateItems(beam.DoFn): 
-    def process(self, element): 
-        seen = set() 
-        for item in element: 
-            item_tuple = (item['ITEM_NBR'], item['PROD_ID']) 
-            if item_tuple not in seen: 
-                seen.add(item_tuple) 
-                yield item
+def coalesce_product_id(row):
+        row['productId'] = row['productId'] if row['productId'] is not None else row['PROD_ID']
+        return row
 
 def run(argv=None):
     options = PipelineOptions(argv)
@@ -100,25 +95,16 @@ def run(argv=None):
     """
 
     with beam.Pipeline(options=options) as p:
-        # Read CDP items
-        cdp_items_list = (p 
-                          | 'Read CDP Items' >> beam.io.ReadFromBigQuery(query=query_cdp_items, use_standard_sql=True, gcs_location = 'gs://outfiles_parquet/offer_bank/temp/',project=google_cloud_options.project)
-        )
-
-        # Drop duplicates based on ITEM_NBR (already done by GroupByKey)
-        cdp_items_list_deduplicated = (
-            cdp_items_list
-            | 'Key by ITEM_NBR' >> beam.Map(lambda x: (x['ITEM_NBR'], (x['PROD_ID'], x['ITEM_NBR'])))
-            | 'Drop Duplicates' >> beam.Distinct()
-            | 'Extract Values' >> beam.Map(lambda x: {'PROD_ID': x[1][0], 'ITEM_NBR': x[1][1]})
-        )
-
-       
 
         # Read clearance items
         clearance_items_metadata = (p 
                                     | 'Read Clearance Items' >> beam.io.ReadFromBigQuery(query=query_clearance_items, use_standard_sql=True,gcs_location = 'gs://outfiles_parquet/offer_bank/temp/',project=google_cloud_options.project)
                                     | 'Format Clearance Items' >> beam.Map(format_clearance_item)
+        )
+
+        # Read CDP items
+        cdp_items_list = (p 
+                          | 'Read CDP Items' >> beam.io.ReadFromBigQuery(query=query_cdp_items, use_standard_sql=True, gcs_location = 'gs://outfiles_parquet/offer_bank/temp/',project=google_cloud_options.project)
         )
 
         # Group by ITEM_NBR and compute distinct PROD_ID count
@@ -129,85 +115,21 @@ def run(argv=None):
             | 'Count Distinct PROD_ID' >> beam.ParDo(GroupByAndCount())
         )
 
- '''    
-        # Join clearance items with CDP items
-        joined_clearance_items = ({
-            'clearance': clearance_items_metadata, 
-            'cdp': cdp_items_list
-        }
-        | 'CoGroupByKey' >> beam.CoGroupByKey()
-        | 'Join Clearance and CDP Items' >> beam.FlatMap(
-            lambda kv: [
-                dict(clearance_item, **{'productId': cdp_item['PROD_ID']}) 
-                for clearance_item in kv[1]['clearance']
-                for cdp_item in kv[1]['cdp']
-            ]
-        )
-        )
-        
-
-        # Join clearance items with grouped CDP items
-        clearance_items_with_product_count = ({
-            'clearance': joined_clearance_items, 
-            'product_count': cdp_items_list_grouped
-        }
-        | 'CoGroupByKey Product Count' >> beam.CoGroupByKey()
-        | 'Join with Product Count' >> beam.FlatMap(
-            lambda kv: [
-                dict(clearance_item, **{'ProductCount': count, 'productItemMappingStatus': (
-                    '1-to-multiple' if count > 1
-                    else 'missing' if clearance_item['productId'] is None
-                    else 'normal'
-                ), 'productId': clearance_item['productId'] if clearance_item['productId'] is not None else ''}) 
-                for clearance_item in kv[1]['clearance']
-                for count in kv[1]['product_count']
-            ]
-        )
+        # Drop duplicates based on ITEM_NBR (already done by GroupByKey)
+        cdp_items_list_deduplicated = (
+            cdp_items_list
+            | 'Key by ITEM_NBR' >> beam.Map(lambda x: (x['ITEM_NBR'], (x['PROD_ID'], x['ITEM_NBR'])))
+            | 'Drop Duplicates' >> beam.Distinct()
+            | 'Extract Values' >> beam.Map(lambda x: {'PROD_ID': x[1][0], 'ITEM_NBR': x[1][1]})
         )
 
-        # Final processing
-        final_clearance_items = (clearance_items_with_product_count
-                                 | 'Create Items Field' >> beam.Map(create_items_field)
-                                 | 'Create Club Overrides Field' >> beam.Map(create_club_overrides_field)
+        # Join the two PCollections
+        clearanced_items_metadata_two = (
+            {'clearance': clearance_items_metadata, 'cdp': cdp_items_list_deduplicated}
+            | 'CoGroupByKey' >> beam.CoGroupByKey()
+            | 'Filter Joined Results' >> beam.FlatMap(lambda x: [
+                {**clearance_item, 'productId': cdp_item['PROD_ID']}
+                for clearance_item in x[1]['clearance']
+                for cdp_item in x[1]['cdp']
+            ])
         )
-
-        # Define schema
-        parquet_schema = pa.schema([
-            ('startDate', pa.string()),
-            ('endDate', pa.string()),
-            ('basePrice', pa.float64()),
-            ('discountValue', pa.float64()),
-            ('itemId', pa.string()),
-            ('clubs', pa.list_(pa.int32())),
-            ('timeZone', pa.string()),
-            ('savingsId', pa.string()),
-            ('savingsType', pa.string()),
-            ('applicableChannels', pa.list_(pa.string())),
-            ('discountType', pa.string()),
-            ('eventTag', pa.int32()),
-            ('members', pa.list_(pa.string())),
-            ('items', pa.list_(pa.struct([
-                ('itemId', pa.string()),
-                ('productId', pa.string()),
-                ('itemType', pa.string()),
-                ('productItemMappingStatus', pa.string())
-            ]))),
-            ('clubOverrides', pa.list_(pa.struct([
-                ('clubNumber', pa.int32()),
-                ('clubStartDate', pa.string()),
-                ('clubEndDate', pa.string())
-            ]))),
-        ])
-
-        # Write to Parquet on local drive (C: drive)
-        output_path = custom_options.output_path
-        final_clearance_items | 'Write to Parquet' >> WriteToParquet(
-            file_path_prefix=os.path.join(output_path, str(date.today())),
-            file_name_suffix='.parquet',
-            schema=parquet_schema
-        )
-'''
-
-# Run the pipeline
-if __name__ == '__main__':
-    run()
