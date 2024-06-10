e7b77198ac97c797771afc5a0403589332066c15
bindhupri <116120716+bindhupri@users.noreply.github.com>
2024-06-09 20:50:41 -0500

    Update offers_dataflow.py

--- offers_dataflow.py
+++ offers_dataflow.py
@@ -22,11 +22,15 @@ def access_secret_version(project_id, secret_id, version_id='latest'):
         raise ValueError("Data corruption detected.")
     return response.payload.data.decode("UTF-8")
 
-output_offer_metadata_path = "gs://outfiles_parquet/offer_bank/offer_result/offer_metadata.json" 
+class GroupByAndCount(beam.DoFn):
+    def process(self, element):
+        item_nbr, prod_ids = element
+        yield {
+            'itemId': item_nbr,
+            'ProductCount': len(set(prod_ids))
+        }
 
-
-# Rename and format columns using the same names as in the PySpark code
-def transform_offer_metadata(offer):
+def transform_and_write_offer_metadata(offer):
     offer['savingsId'] = str(offer['offer_id'])
     offer['savingsType'] = offer['offer_source']
     offer['startDate'] = offer['start_datetime'].strftime("%Y-%m-%d %H:%M:%S") if offer['start_datetime'] else ""
@@ -57,11 +61,32 @@ def transform_offer_metadata(offer):
     del offer['discount_value']
     del offer['applicable_channel']
     del offer['club_list'] 
+
+    # cdp_items_list = get_product_item_mapping(spark)
+    print("Fetching active products from cdp tables")
+    query_cdp_items = """select t1.PROD_ID, t1.ITEM_NBR FROM `prod-sams-cdp.US_SAMS_PRODUCT360_CDP_VM.CLUB_ITEM_GRP` t1
+                join `prod-sams-cdp.US_SAMS_PRODUCT360_CDP_VM.PROD` t2
+                on t1.PROD_ID = t2.PROD_ID
+                where t2.PROD_STATUS_CD = 'ACTIVE'"""
     
-    return offer 
- 
+    cdp_items_list = (p 
+                          | 'Read CDP Items' >> beam.io.ReadFromBigQuery(query=query_cdp_items, use_standard_sql=True, gcs_location = 'gs://outfiles_parquet/offer_bank/temp/',project=google_cloud_options.project)
+        )
+
+    # Group by ITEM_NBR and compute distinct PROD_ID count
+    cdp_items_list_grouped = (
+            cdp_items_list
+            | 'Pair with ITEM_NBR' >> beam.Map(lambda x: (x['ITEM_NBR'], x['PROD_ID']))
+            | 'Group by ITEM_NBR' >> beam.GroupByKey()
+            | 'Count Distinct PROD_ID' >> beam.ParDo(GroupByAndCount())
+            | 'Key by itemId for cdp data' >> beam.Map(lambda row: (row['itemId'], row))
+        )
+         
+
+output_offer_metadata_path = "gs://outfiles_parquet/offer_bank/offer_result/offer_metadata.json" 
+
 # Function to fetch offer metadata 
-def fetch_offer_metadata(jdbc_url, jdbc_user, jdbc_password):
+def write_broadreach_offers(jdbc_url, jdbc_user, jdbc_password):
     query = """
         select o.offer_id, o.offer_source, o.start_datetime, o.end_datetime, o.time_zone, o.discount_type, o.discount_value,
         o.applicable_channel, o.club_list, o.labels, oi.item_number, oi.product_id, oi.item_type, mo.membership_id,
@@ -113,8 +138,6 @@ def run(argv=None):
     project = custom_options.project_id
     env = custom_options.env
     savings_ds_bucket = custom_options.savings_ds_bucket
-
-    # Ensure the project is set in pipeline options
     google_cloud_options = pipeline_options.view_as(GoogleCloudOptions)
     google_cloud_options.project = project
  
@@ -124,13 +147,14 @@ def run(argv=None):
         jdbc_password = access_secret_version(project, f'{env}PostgresRWPassword', 'latest')
     except Exception as error:
         raise Exception(f"Error while fetching secrets from secret manager: {error}")
+    
 
     with beam.Pipeline(options=pipeline_options) as p:
         # Fetch offer metadata
         offer_metadata = (
             p
-            | 'FetchOfferMetadata' >> fetch_offer_metadata(jdbc_url, jdbc_user, jdbc_password) 
-			| 'TransformOfferMetadata' >> beam.Map(transform_offer_metadata)
+            | 'broadreachoffersMetadata' >> write_broadreach_offers(jdbc_url, jdbc_user, jdbc_password) 
+            | 'TransformOfferMetadata' >> beam.Map(transform_and_write_offer_metadata)
         )
 
         offer_metadata_written = (
