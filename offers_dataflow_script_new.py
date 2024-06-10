1191945c45f400a6f7b17da8319546d3a921b9bd
bindhupri <116120716+bindhupri@users.noreply.github.com>
2024-06-11 00:23:40 +0530

    Update offers_dataflow_script_new.py

--- offers_dataflow_script_new.py
+++ offers_dataflow_script_new.py
@@ -30,41 +30,109 @@ class GroupByAndCount(beam.DoFn):
             'ProductCount': len(set(prod_ids))
         }
 
-def transform_and_write_offer_metadata(offer):
-    offer['savingsId'] = str(offer['offer_id'])
-    offer['savingsType'] = offer['offer_source']
-    offer['startDate'] = offer['start_datetime'].strftime("%Y-%m-%d %H:%M:%S") if offer['start_datetime'] else ""
-    offer['endDate'] = offer['end_datetime'].strftime("%Y-%m-%d %H:%M:%S") if offer['end_datetime'] else ""
-    offer['timeZone'] = offer['time_zone']
-    offer['discountType'] = offer['discount_type']
-    offer['discountValue'] = float(offer['discount_value'])  # Ensure proper handling of discount_value
-    offer['applicableChannels'] = offer['applicable_channel']
-    offer['clubs'] = offer['club_list']
-    offer['exclusive_club_startdate'] = offer['exclusive_club_startdate'].strftime("%Y-%m-%d %H:%M") if offer['exclusive_club_startdate'] else ""
-    offer['exclusive_club_enddate'] = offer['exclusive_club_enddate'].strftime("%Y-%m-%d %H:%M") if offer['exclusive_club_enddate'] else ""
-    offer['labels'] = offer['labels'] if offer['labels'] else []
-    offer['eventTag'] = 2 if 'event' in offer['labels'] else 1 if 'ISB' in offer['labels'] else 0
-    offer['basePrice'] = 0.0
-    offer['exclusive_club_number'] = int(offer['exclusive_club_number']) if offer['exclusive_club_number'] else 0
-    offer['itemId'] = offer['item_number']
-    offer['productId'] = offer['product_id'] if offer['product_id'] else ""
-    offer['itemType'] = offer['item_type'] if offer['item_type'] else "DiscountedItem"
-    offer['productItemMappingStatus'] = ""
+def transform_and_write_offer_metadata(offer_metadata,p,pipeline_options):
+    # Apply transformations
+    transformed_data = (
+        offer_metadata
+        # Apply column renaming
+        | 'RenameColumns' >> beam.Map(lambda row: {
+            'savingsId': row['offer_id'],
+            'savingsType': row['offer_source'],
+            'startDate': row['start_datetime'],
+            'endDate': row['end_datetime'],
+            'timeZone': row['time_zone'],
+            'discountType': row['discount_type'],
+            'discountValue': row['discount_value'],
+            'applicableChannels': row['applicable_channel'],
+            'clubs': row['club_list']
+        })
+        # Set default values
+        | 'SetDefaultValues' >> beam.Map(lambda row: {
+            **row,
+            'exclusive_club_startdate': row.get('exclusive_club_startdate', ''),
+            'exclusive_club_enddate': row.get('exclusive_club_enddate', ''),
+            'labels': row.get('labels', []),
+            'eventTag': 0,
+            'basePrice': 0
+        })
+        | 'FillNullValues' >> beam.Map(lambda row: {
+            **row,
+            'exclusive_club_number': row.get('exclusive_club_number', 0),
+            'exclusive_club_startdate': row.get('exclusive_club_startdate', ''),
+            'exclusive_club_enddate': row.get('exclusive_club_enddate', ''),
+            'item_type': row.get('item_type', 'DiscountedItem')
+        })
+        # Convert column data types
+        | 'ConvertDataTypes' >> beam.Map(lambda row: {
+            **row,
+            'basePrice': float(row['basePrice']),  # Assuming basePrice is float
+            'discountValue': float(row['discountValue']),  # Assuming discountValue is float
+            'savingsId': str(row['savingsId']),
+            'startDate': str(row['startDate']),
+            'endDate': str(row['endDate']),
+            'exclusive_club_number': int(row.get('exclusive_club_number', 0)),  # Assuming exclusive_club_number is int
+            'item_number': int(row.get('item_number', 0))  # Assuming item_number is int
+        })
+        # Set eventTag based on labels
+        | 'SetEventTag' >> beam.Map(lambda row: {
+            **row,
+            'eventTag': 2 if 'event' in row['labels'] else (1 if 'ISB' in row['labels'] else 0)
+        })
+        # Additional transformations...
+    ) 
+    offer_metadata_written = (
+            transformed_data 
+            | 'Write offer Metadata to GCS' >> beam.io.WriteToText(output_offer_metadata_path, file_name_suffix=".json")
+    )
+    # cdp_items_list = get_product_item_mapping(spark)
+    print("Fetching active products from cdp tables")
+    query_cdp_items = """select t1.PROD_ID, t1.ITEM_NBR FROM `prod-sams-cdp.US_SAMS_PRODUCT360_CDP_VM.CLUB_ITEM_GRP` t1
+                join `prod-sams-cdp.US_SAMS_PRODUCT360_CDP_VM.PROD` t2
+                on t1.PROD_ID = t2.PROD_ID
+                where t2.PROD_STATUS_CD = 'ACTIVE'"""
     
-    # Delete the original keys used for renaming
-    del offer['offer_id']
-    del offer['offer_source']
-    del offer['start_datetime']
-    del offer['end_datetime']
-    del offer['time_zone']
-    del offer['discount_type']
-    del offer['discount_value']
-    del offer['applicable_channel']
-    del offer['club_list'] 
+    cdp_items_list = (p 
+                          | 'Read CDP Items' >> beam.io.ReadFromBigQuery(query=query_cdp_items, use_standard_sql=True, gcs_location = 'gs://outfiles_parquet/offer_bank/temp/',project=pipeline_options.view_as(GoogleCloudOptions).project)
+        )
 
-    return offer
+    # Group by ITEM_NBR and compute distinct PROD_ID count
+    cdp_items_list_grouped = (
+            cdp_items_list
+            | 'Pair with ITEM_NBR' >> beam.Map(lambda x: (x['ITEM_NBR'], x['PROD_ID']))
+            | 'Group by ITEM_NBR' >> beam.GroupByKey()
+            | 'Count Distinct PROD_ID' >> beam.ParDo(GroupByAndCount())
+            | 'Rename item_number' >> beam.Map(lambda row: {'item_number': row['ITEM_NBR'], 'ProductCount': row['ProductCount']})
+            | 'Key by itemId for cdp data' >> beam.Map(lambda row: (row['item_number'], row))
+        )
          
-
+    # Drop duplicates based on ITEM_NBR (already done by GroupByKey)
+    cdp_items_list_deduplicated = (
+            cdp_items_list
+            | 'Key by ITEM_NBR' >> beam.Map(lambda x: (x['ITEM_NBR'], (x['PROD_ID'], x['ITEM_NBR'])))
+            | 'Drop Duplicates' >> beam.Distinct()
+            | 'Extract Values' >> beam.Map(lambda x: {'PROD_ID': x[1][0], 'ITEM_NBR': x[1][1]})
+            | 'Key by ITEM_NBR for cdp data' >> beam.Map(lambda row: (row['ITEM_NBR'], row))
+        )
+    # join to fetch item-product mapping only for relevant items
+    transformed_offer_metadata1 = (
+            {'offers': transformed_data, 'cdp': cdp_items_list_deduplicated}
+            | 'CoGroupByKey' >> beam.CoGroupByKey()
+            | 'Filter Joined Results' >> beam.FlatMap(lambda x: [
+                {**clearance_item, 'productId': cdp_item['PROD_ID']}
+                for clearance_item in x[1]['offers']
+                for cdp_item in x[1]['cdp']
+                if clearance_item['item_number'] == cdp_item['ITEM_NBR'] ])
+        )
+    # Join the resulting PCollection with cdp_items_list_grouped 
+        transformed_offer_metadata3 = ( 
+            {'offers1': transformed_offer_metadata1, 'cdp_grouped': cdp_items_list_grouped} 
+            | 'CoGroupByKey Final' >> beam.CoGroupByKey() 
+            | 'Filter and Flatten Final Join' >> beam.FlatMap(lambda x: [ 
+                {**clearance_item, **{'ProductCount': grouped_item['ProductCount']}} 
+                for clearance_item in x[1]['offers1'] 
+                for grouped_item in x[1]['cdp_grouped'] 
+                if clearance_item['item_number'] == grouped_item['item_number'] ]) )  
+                
 output_offer_metadata_path = "gs://outfiles_parquet/offer_bank/offer_result/offer_metadata.json" 
 
 # Function to fetch offer metadata 
@@ -136,17 +204,9 @@ def run(argv=None):
         offer_metadata = (
             p
             | 'broadreachoffersMetadata' >> write_broadreach_offers(jdbc_url, jdbc_user, jdbc_password) 
-            | 'TransformOfferMetadata' >> beam.Map(transform_and_write_offer_metadata)
         )
-
-        # cdp_items_list = get_product_item_mapping(spark)
-    
-
-        offer_metadata_written = (
-            offer_metadata 
-            | 'Write offer Metadata to GCS' >> beam.io.WriteToText(output_offer_metadata_path, file_name_suffix=".json")
-        )
-
+        # Apply transformations and write to destination
+        transform_and_write_offer_metadata(offer_metadata,p,pipeline_options)
 
 if __name__ == '__main__':
     run()
