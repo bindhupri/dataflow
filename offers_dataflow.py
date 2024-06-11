37ea6d8004801c4750f26f411fcc2d5537435bff
bindhupri <116120716+bindhupri@users.noreply.github.com>
2024-06-11 07:18:17 -0500

    Update offers_dataflow.py

--- offers_dataflow.py
+++ offers_dataflow.py
@@ -80,10 +80,8 @@ def transform_and_write_offer_metadata(offer_metadata,p,pipeline_options):
         })
         # Additional transformations...
     ) 
-    offer_metadata_written = (
-            transformed_data 
-            | 'Write offer Metadata to GCS' >> beam.io.WriteToText(output_offer_metadata_path, file_name_suffix=".json")
-    )
+    offer_metadata_written = transformed_data | 'Write offer Metadata to GCS' >> beam.io.WriteToText(output_offer_metadata_path, file_name_suffix=".json")
+    
     # cdp_items_list = get_product_item_mapping(spark)
     print("Fetching active products from cdp tables")
     query_cdp_items = """select t1.PROD_ID, t1.ITEM_NBR FROM `prod-sams-cdp.US_SAMS_PRODUCT360_CDP_VM.CLUB_ITEM_GRP` t1
@@ -101,11 +99,46 @@ def transform_and_write_offer_metadata(offer_metadata,p,pipeline_options):
             | 'Pair with ITEM_NBR' >> beam.Map(lambda x: (x['ITEM_NBR'], x['PROD_ID']))
             | 'Group by ITEM_NBR' >> beam.GroupByKey()
             | 'Count Distinct PROD_ID' >> beam.ParDo(GroupByAndCount())
-            | 'Key by itemId for cdp data' >> beam.Map(lambda row: (row['itemId'], row))
+            | 'Rename item_number' >> beam.Map(lambda row: {'item_number': row['itemId'], 'ProductCount': row['ProductCount']})
+            | 'Key by itemId for cdp data' >> beam.Map(lambda row: (row['item_number'], row))
         )
-         
 
+    cdp_items_list_grouped_written = cdp_items_list_grouped | ' Write offer Metadata to GCS' >> beam.io.WriteToText(output_cdp_items_list_grouped_path, file_name_suffix=".json")
+    
+    # Drop duplicates based on ITEM_NBR (already done by GroupByKey)
+    cdp_items_list_deduplicated = (
+            cdp_items_list
+            | 'Key by ITEM_NBR' >> beam.Map(lambda x: (x['ITEM_NBR'], (x['PROD_ID'], x['ITEM_NBR'])))
+            | 'Drop Duplicates' >> beam.Distinct()
+            | 'Extract Values' >> beam.Map(lambda x: {'PROD_ID': x[1][0], 'ITEM_NBR': x[1][1]})
+            | 'Key by ITEM_NBR for cdp data' >> beam.Map(lambda row: (row['ITEM_NBR'], row))
+        )
+    
+    cdp_items_list_deduplicated_written = cdp_items_list_deduplicated | 'Write offer Metadata to GCS.' >> beam.io.WriteToText(output_cdp_items_list_deduplicated_path, file_name_suffix=".json")
+'''    
+    # join to fetch item-product mapping only for relevant items
+    transformed_offer_metadata1 = (
+            {'offers': transformed_data, 'cdp': cdp_items_list_deduplicated}
+            | 'CoGroupByKey' >> beam.CoGroupByKey()
+            | 'Filter Joined Results' >> beam.FlatMap(lambda x: [
+                {**clearance_item, 'productId': cdp_item['PROD_ID']}
+                for clearance_item in x[1]['offers']
+                for cdp_item in x[1]['cdp']
+                if clearance_item['item_number'] == cdp_item['ITEM_NBR'] ])
+        )
+    # Join the resulting PCollection with cdp_items_list_grouped 
+    transformed_offer_metadata3 = ( 
+            {'offers1': transformed_offer_metadata1, 'cdp_grouped': cdp_items_list_grouped} 
+            | 'CoGroupByKey Final' >> beam.CoGroupByKey() 
+            | 'Filter and Flatten Final Join' >> beam.FlatMap(lambda x: [ 
+                {**clearance_item, **{'ProductCount': grouped_item['ProductCount']}} 
+                for clearance_item in x[1]['offers1'] 
+                for grouped_item in x[1]['cdp_grouped'] 
+                if clearance_item['item_number'] == grouped_item['item_number'] ]) )  
+ '''               
 output_offer_metadata_path = "gs://outfiles_parquet/offer_bank/offer_result/offer_metadata.json" 
+output_cdp_items_list_grouped_path = "gs://outfiles_parquet/offer_bank/offer_result/cdp_items_list_grouped.json" 
+output_cdp_items_list_deduplicated_path = "gs://outfiles_parquet/offer_bank/offer_result/cdp_items_list_deduplicated.json"
 
 # Function to fetch offer metadata 
 def write_broadreach_offers(jdbc_url, jdbc_user, jdbc_password):
