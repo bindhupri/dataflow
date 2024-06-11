59543f4bf88b8db0d4c0321f6d1e47b080129c2a
bindhupri <116120716+bindhupri@users.noreply.github.com>
2024-06-12 01:57:46 +0530

    Update offers_dataflow_script_new.py

--- offers_dataflow_script_new.py
+++ offers_dataflow_script_new.py
@@ -72,7 +72,8 @@ def transform_and_write_offer_metadata(offer_metadata, p, pipeline_options):
             'discountType': row['discount_type'],
             'discountValue': row['discount_value'],
             'applicableChannels': row['applicable_channel'],
-            'clubs': row['club_list']
+            'clubs': row['club_list'],
+            'item_number': row['item_number']
         })
         # Set default values
         | 'SetDefaultValues' >> beam.Map(lambda row: {
@@ -98,8 +99,7 @@ def transform_and_write_offer_metadata(offer_metadata, p, pipeline_options):
             'savingsId': str(row['savingsId']),
             'startDate': str(row['startDate']),
             'endDate': str(row['endDate']),
-            'exclusive_club_number': int(row['exclusive_club_number']),  # Assuming exclusive_club_number is int
-            'item_number': str(row['item_number'])  # Assuming item_number is int
+            'exclusive_club_number': int(row['exclusive_club_number']),  # Assuming exclusive_club_number is int 
         })
         # Set eventTag based on labels
         | 'SetEventTag' >> beam.Map(lambda row: {
